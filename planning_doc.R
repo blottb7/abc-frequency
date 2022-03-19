5442a58e308037027e509d9feaedd67e55b60f12
Ben <benblott@gmail.com>
2022-03-19 12:31:24 -0500

    Planning steps.

---
+++ planning_doc.R
@@ -0,0 +1,21 @@
+# Outputs
+# I. % checklists recording species
+# A. overall
+# 1. ABC
+# 2. Jefferson Co (do not include ABC for any Jeff. Co.)
+#. a) include all non ABC instead
+# 3. Lake Mills
+# 4. top 10 hotspots vs ABC
+# B. by month
+# 1. ABC
+# 2. Jefferson Co
+
+# II. Comparisons
+# A. list ranked by Adams having more
+# B. list ranked by Jefferson Co having more
+# C. comparisons by taxonomic
+
+# Notes:
+# Would probably be useful if not best to compare ABC to more than just
+# Zeloski... pick, say, the top 10 hot spots, or all spots with, say,
+# 200+ species. Might get some truer percentages.
